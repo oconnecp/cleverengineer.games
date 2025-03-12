35c699084d0cc78b3d990bf4fddea4d844e8ec45
oconnecp <oconnecp@gmail.com>
2025-03-12 16:34:17 -0700

    changing the name to the domain we will use

--- frontend/src/components/Boggle/BoggleWordList.tsx
+++ frontend/src/components/Boggle/BoggleWordList.tsx
@@ -4,7 +4,7 @@ interface BoggleWordListProps {
   words: string[]
 }
 
-const boggleWordListStyle : React.CSSProperties = {
+const boggleWordListStyle: React.CSSProperties = {
   height: "100%",
   display: "flex",
   flexDirection: "column",
@@ -12,14 +12,23 @@ const boggleWordListStyle : React.CSSProperties = {
   margin: "0 0 5px 0",
 }
 
-const h2Style : React.CSSProperties = {
+const h2Style: React.CSSProperties = {
   textAlign: "center"
 }
 
-const scrollingStyle : React.CSSProperties = {
+const scrollingStyle: React.CSSProperties = {
   display: "flex",
   height: "100%",
   overflow: "auto"
+}
+
+const spacerStyle: React.CSSProperties = {
+  width: "25%",
+}
+const listStyle: React.CSSProperties = {
+  width: "100%",
+  listStyleType: "none",
+  textAlign: "center",
 }
 
 export const BoggleWordList: React.FC<BoggleWordListProps> = ({ words }) => {
@@ -27,11 +36,18 @@ export const BoggleWordList: React.FC<BoggleWordListProps> = ({ words }) => {
     <div style={boggleWordListStyle}>
       <h2 style={h2Style}>{words.length > 0 ? 'Words Found:' : 'Find Words by swiping'}</h2>
       <div style={scrollingStyle}>
-        <ul>
+        <div style={spacerStyle}/>
+        <ul style={listStyle}>
           {words.map((word, index) => (
-            <li key={index}>{word}</li>
+            index % 2 === 0 && (<li key={index}>{word}</li>)
           ))}
         </ul>
+        <ul style={listStyle}>
+          {words.map((word, index) => (
+            index % 2 === 1 && (<li key={index}>{word}</li>)
+          ))}
+        </ul>
+        <div style={spacerStyle}/>
       </div>
     </div>
   )
