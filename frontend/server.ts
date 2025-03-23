aafa23049ac45fa3ca0aff3acc2a8c149590be6d
oconnecp <oconnecp@gmail.com>
2025-03-23 16:16:17 -0700

    fixing path

--- frontend/server.ts
+++ frontend/server.ts
@@ -8,8 +8,8 @@ const app = express();
 
 
 // Define __filename and __dirname for ES modules
-// const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname('./');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Configure CORS to allow requests from the backend
 if (ADD_CORS) {
