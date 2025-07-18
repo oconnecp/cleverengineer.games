54075c4531671e1107c65a96c9f0aa37a558c16f
oconnecp <oconnecp@gmail.com>
2025-07-18 15:54:52 -0700

    change from upper to lowercase

--- production/initializesecrets.js
+++ production/initializesecrets.js
@@ -20,10 +20,12 @@ if (!fs.existsSync(secretsDir)) {
 // Create each secret file if it doesn't exist
 secretFiles.forEach(filename => {
   const filePath = path.join(secretsDir, filename);
+  const lowercaseFilePath = path.join(secretsDir, filename.toLocaleLowerCase);
   if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, '', { encoding: 'utf-8' });
     console.log(`Created empty secret file: ${filename}`);
   } else {
     console.log(`Secret file already exists: ${filename}`);
+    fs.renameSync(filePath, filePath.toLowerCase);
   }
 });
