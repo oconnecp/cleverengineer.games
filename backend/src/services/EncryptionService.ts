4f748cfab9b9fb025a5a1b5e5ee6e41f8d78b7da
oconnecp <oconnecp@gmail.com>
2025-07-19 14:39:39 -0700

    cleaned up the encryption key work

--- backend/src/services/EncryptionService.ts
+++ backend/src/services/EncryptionService.ts
@@ -2,7 +2,7 @@ import crypto from 'crypto';
 
 export const encrypt = (text: string, secret: string): string => {
   const iv = crypto.randomBytes(16); // Generate a random initialization vector
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secret, 'hex'), iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secret, 'utf-8'), iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return iv.toString('hex') + ':' + encrypted; // Include IV with the encrypted text
@@ -11,7 +11,7 @@ export const encrypt = (text: string, secret: string): string => {
 export const decrypt = (encryptedText: string, secret: string): string => {
   const [ivHex, encryptedData] = encryptedText.split(':'); // Split IV and encrypted data
   const iv = Buffer.from(ivHex, 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret, 'hex'), iv);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret, 'utf-8'), iv);
   let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
